e68d095b99f63757d7fe689c6d3d602b136bb2bc
unknown <esaeed@cayesh.me>
2024-08-13 23:12:21 +0300

    3

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://admin:password@192.168.80.4:27017').then(console.log
 
 app.get('/', (req, res) => {
     console.log("here555555");
-    res.send('Hello World 2!');
+    res.send('Hello World 233!');
 });
 
 app.listen(port, () => {
