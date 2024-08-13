6f6812020eaa9dbe5ca18a6b37fda41c9a62dd27
unknown <esaeed@cayesh.me>
2024-08-13 22:44:01 +0300

    first commit

---
+++ prisma/migrations/20240812083935_reinit_2/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `Post` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL,
+    MODIFY `title` VARCHAR(255) NOT NULL;
+
+-- CreateTable
+CREATE TABLE `Profile` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `bio` VARCHAR(191) NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Profile_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Profile` ADD CONSTRAINT `Profile_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// const { PrismaClient } = require('@prisma/client')
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+// const prisma = new PrismaClient();
+
+mongoose.connect('mongodb://admin:password@192.168.80.4:27017').then(console.log('connected')).catch((e) => {
+    console.log(e);
+});
+
+app.get('/', (req, res) => {
+    console.log("here33");
+    res.send('Hello World 2!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
